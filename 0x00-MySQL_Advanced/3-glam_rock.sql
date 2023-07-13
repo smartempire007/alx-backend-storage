7dfd34cbcd27bae73052c314764aa1a19c2d925c
Nwadike Joseph <jcyoung.mjc44@gmail.com>
2023-07-13 23:31:18 +0100

    fix task3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -6,12 +6,8 @@
 -- Column names must be: band_name and lifespan (in years until 2022 - please use 2022 instead of YEAR(CURDATE()))
 -- You should use attributes formed and split for computing the lifespan
 -- Your script can be executed on any database
-SELECT
-    band_name,
-    (IFNULL(split, '2022') - formed) AS lifespan
-FROM
-    metal_bands
-WHERE
-    style LIKE '%Glam rock%'
-ORDER BY
-    lifespan DESC;
+
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+FROM metal_bands
+WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+ORDER BY lifespan DESC;
