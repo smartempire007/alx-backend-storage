096d281d66bcc17431397d0d5d8d4bf677764b5f
Nwadike Joseph <jcyoung.mjc44@gmail.com>
2023-07-13 22:07:15 +0100

    0-uniq_users.sql task committed

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- Write an SQL script that create a table named "users" with the following fields:
+-- id, name, email
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE
+);
