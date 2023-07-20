b70329cc4949cd2edc42ab1d103151bb681c5823
Nwadike Joseph <jcyoung.mjc44@gmail.com>
2023-07-20 09:09:32 +0100

    task 0,1,2,3,4 committed

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+'''Create a Cache class. In the __init__ method, store an instance of the Redis
+client as a private variable named _redis (using redis.Redis()) and flush the
+instance using flushdb.
+
+Create a store method that takes a data argument and returns a string.
+The method should generate a random key (e.g. using uuid), store the input
+data in Redis using the random key and return the key.
+
+Type-annotate store correctly. Remember that data can be a str, bytes,
+int or float.
+'''
+import redis
+from typing import Union, Optional, Callable
+from uuid import uuid4, UUID
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    '''Counts the number of times a method is called'''
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        '''Wrapper function'''
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    '''Stores the history of inputs and outputs for a 
+    particular function'''
+    input_list_key = method.__qualname__ + ":inputs"
+    output_list_key = method.__qualname__ + ":outputs"
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        '''Wrapper function'''
+        self._redis.rpush(input_list_key, str(args))
+        output = method(self, *args, **kwargs)
+        self._redis.rpush(output_list_key, str(output))
+        return output
+    return wrapper
+
+
+def replay(method: Callable):
+    '''Displays the history of calls of a particular function'''
+    cache = redis.Redis()
+    name = method.__qualname__
+    input_list_key = name + ":inputs"
+    output_list_key = name + ":outputs"
+    assert cache.llen(input_list_key) and cache.llen(output_list_key)
+    inputs = cache.lrange(input_list_key, 0, cache.llen(input_list_key))
+    outputs = cache.lrange(output_list_key, 0, cache.llen(output_list_key))
+
+    print("{} was called {} times:".format(name, cache.llen(input_list_key)))
+
+    for input, output in zip(inputs, outputs):
+        print("{}(*{}) -> {}".format(name, input.decode('utf-8'),
+                                     output.decode('utf-8')))
+
+
+class Cache:
+    '''A Cache class using Redis as a data store'''
+
+    def __init__(self) -> None:
+        '''Initialize Cache instance'''
+        self._redis = redis.Redis(host='localhost', port=6379, db=0)
+        self._redis.flushdb()
+
+    @call_history
+    @count_calls
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        '''Store data in Redis'''
+        key = str(uuid4())
+        self._redis.set(key, data)
+        return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        '''Get data from Redis'''
+        data = self._redis.get(key)
+        if fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        '''Convert bytes to str'''
+        data = self._redis.get(key)
+        return data.decode('utf-8')
+
+    def get_int(self, key: str) -> int:
+        '''Convert bytes to int'''
+        data = self._redis.get(key)
+        try:
+            data = int(data.decode('utf-8'))
+        except ValueError:
+            data = 0
+        return data
---
+++ 0x02-redis_basic/main.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+import redis
+
+Cache = __import__('exercise').Cache
+replay = __import__('exercise').replay
+
+cache = Cache()
+
+data = b"hello"
+key = cache.store(data)
+print(key)
+
+local_redis = redis.Redis()
+print(local_redis.get(key))
+
+TEST_CASES = {
+    b"foo": None,
+    123: int,
+    "bar": lambda d: d.decode("utf-8")
+}
+
+for value, fn in TEST_CASES.items():
+    key = cache.store(value)
+    assert cache.get(key, fn=fn) == value
+
+TEST_CASES_2 = ['July', '19th', 2023]
+
+for value in TEST_CASES_2:
+    key = cache.store(value)
+    print(f'[{key}] str: {cache.get_int(key)} int: {cache.get_str(key)}')
+
+cache = Cache()
+
+cache.store(b"first")
+print(cache.get(cache.store.__qualname__))
+
+cache.store(b"second")
+cache.store(b"third")
+print(cache.get(cache.store.__qualname__))
+
+cache = Cache()
+
+s1 = cache.store("first")
+print(s1)
+s2 = cache.store("secont")
+print(s2)
+s3 = cache.store("third")
+print(s3)
+
+inputs = cache._redis.lrange(
+    "{}:inputs".format(cache.store.__qualname__), 0, -1)
+outputs = cache._redis.lrange(
+    "{}:outputs".format(cache.store.__qualname__), 0, -1)
+
+print("inputs: {}".format(inputs))
+print("outputs: {}".format(outputs))
+
+
+cache = Cache()
+
+cache.store("foo")
+cache.store("bar")
+cache.store(42)
+replay(cache.store)
